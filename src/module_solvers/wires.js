0fe03f21da8bf4e640fca4f1834144d7e0f6f153
Danny Paez <dpaez16@yahoo.com>
2023-07-22 09:03:22 -0700

    wires solver class

--- src/module_solvers/wires.js
+++ src/module_solvers/wires.js
@@ -6,9 +6,9 @@ export default class WiresSolver {
         const num_blue_wires = wires.filter(wire => wire === WireColor.BLUE).length;
         const last_blue_wire_pos = wires.lastIndexOf(WireColor.BLUE);
 
-		if (!wires.includes(WireColor.RED)) {
+        if (!wires.includes(WireColor.RED)) {
             return 2;
-        } else if (wires[2] === WireColor.WHITE) {
+        } else if (wires.slice(-1) === WireColor.WHITE) {
             return 3;
         } else if (num_blue_wires > 1) {
             return last_blue_wire_pos;
@@ -18,34 +18,68 @@ export default class WiresSolver {
     }
 
     static #helper_4_wires(wires, last_serial_number_digit) {
-    	const num_red_wires = wires.filter(wire => wire === WireColor.RED).length;
+        const num_red_wires = wires.filter(wire => wire === WireColor.RED).length;
         const num_blue_wires = wires.filter(wire => wire === WireColor.BLUE).length;
         const num_yellow_wires = wires.filter(wire => wire === WireColor.YELLOW).length;
-    	const last_red_wire_pos = wires.lastIndexOf(WireColor.RED);
+        const last_red_wire_pos = wires.lastIndexOf(WireColor.RED);
 
-    	if (num_red_wires > 1 && last_serial_number_digit % 2 === 1) {
-			return 4;
-    	} else if (wires[3] === WireColor.YELLOW && num_red_wires === 0) {
-			return 1;
-    	} else if (num_blue_wires === 1) {
-    		return 1;
-    	} else if (num_yellow_wires > 1) {
-			return 4;
-    	} else {
-			return 2;
-    	}
+        if (num_red_wires > 1 && last_serial_number_digit % 2 === 1) {
+            return last_red_wire_pos;
+        } else if (wires.slice(-1) === WireColor.YELLOW && num_red_wires === 0) {
+            return 1;
+        } else if (num_blue_wires === 1) {
+            return 1;
+        } else if (num_yellow_wires > 1) {
+            return 4;
+        } else {
+            return 2;
+        }
+    }
+
+    static #helper_5_wires(wires, last_serial_number_digit) {
+        const num_red_wires = wires.filter(wire => wire === WireColor.RED).length;
+        const num_black_wires = wires.filter(wire => wire === WireColor.BLACK).length;
+        const num_yellow_wires = wires.filter(wire => wire === WireColor.YELLOW).length;
+
+        if (wires.slice(-1) === WireColor.BLACK && last_serial_number_digit % 2 === 1) {
+            return 4;
+        } else if (num_red_wires === 1 && num_yellow_wires > 1) {
+            return 1;
+        } else if (num_black_wires === 0) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+    static #helper_6_wires(wires, last_serial_number_digit) {
+        const num_red_wires = wires.filter(wire => wire === WireColor.RED).length;
+        const num_white_wires = wires.filter(wire => wire === WireColor.WHITE).length;
+        const num_yellow_wires = wires.filter(wire => wire === WireColor.YELLOW).length;
+
+        if (num_yellow_wires === 0 && last_serial_number_digit % 2 === 1) {
+            return 3;
+        } else if (num_yellow_wires === 1 && num_white_wires > 1) {
+            return 4;
+        } else if (num_red_wires === 0) {
+            return 6;
+        } else {
+            return 4;
+        }
     }
 
     static solve(wires, last_serial_number_digit) {
-    	const num_wires = wires.length;
+        const num_wires = wires.length;
         wires = wires.map(wire => WireColor.fromString(wire));
 
         if (num_wires === 3) {
             return this.#helper_3_wires(wires);
         } else if (num_wires === 4) {
-        	return this.#helper_4_wires(wires, last_serial_number_digit);
+            return this.#helper_4_wires(wires, last_serial_number_digit);
+        } else if (num_wires === 5) {
+            return this.#helper_5_wires(wires, last_serial_number_digit);
         } else {
-        	return -1;
+            return this.#helper_6_wires(wires, last_serial_number_digit);
         }
     }
 };
