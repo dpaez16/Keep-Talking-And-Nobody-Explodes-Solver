60e7609febbf3792787ee97bb85091006c0dab3e
Danny Paez <dpaez16@yahoo.com>
2023-07-22 10:19:25 -0700

    keypad solver

---
+++ src/module_solvers/keypads.js
@@ -0,0 +1,39 @@
+import KeypadSymbol from '../types/keypad_symbol';
+
+export default class KeypadSolver {
+    static #COLUMNS = [
+        [KeypadSymbol.MIRROR, KeypadSymbol.FUNNY_LETTER_A, KeypadSymbol.LAMBDA, KeypadSymbol.WEIRD_MIRRORED_N, KeypadSymbol.KITTY, KeypadSymbol.LETTER_H, KeypadSymbol.BACKWARD_C_WITH_DOT],
+        [KeypadSymbol.BACKWARD_E_WITH_UMLAUT, KeypadSymbol.MIRROR, KeypadSymbol.BACKWARD_C_WITH_DOT, KeypadSymbol.FANCY_Q, KeypadSymbol.HOLLOW_STAR, KeypadSymbol.LETTER_H, KeypadSymbol.UPSIDE_DOWN_QUESTION_MARK],
+        [KeypadSymbol.COPYRIGHT, KeypadSymbol.BALLSACK, KeypadSymbol.FANCY_Q, KeypadSymbol.MIRRORED_PAIR_OF_KS, KeypadSymbol.WEIRD_SCRIPT_R, KeypadSymbol.LAMBDA, KeypadSymbol.HOLLOW_STAR],
+        [KeypadSymbol.WEIRD_6, KeypadSymbol.BACKWARDS_PARAGRAPH, KeypadSymbol.LETTER_B_WITH_CROSS, KeypadSymbol.KITTY, KeypadSymbol.MIRRORED_PAIR_OF_KS, KeypadSymbol.UPSIDE_DOWN_QUESTION_MARK, KeypadSymbol.SMILEY_FACE],
+        [KeypadSymbol.PSI, KeypadSymbol.SMILEY_FACE, KeypadSymbol.LETTER_B_WITH_CROSS, KeypadSymbol.C_WITH_DOT, KeypadSymbol.BACKWARDS_PARAGRAPH, KeypadSymbol.THREE_WITH_TAIL, KeypadSymbol.FULL_STAR],
+        [KeypadSymbol.WEIRD_6, KeypadSymbol.BACKWARD_E_WITH_UMLAUT, KeypadSymbol.PUZZLE_PIECE, KeypadSymbol.AE, KeypadSymbol.PSI, KeypadSymbol.FORMAL_MIRRORED_N, KeypadSymbol.OMEGA]
+    ];
+
+    static #symbols_in_column(keypad_symbols, column) {
+        for (let keypad_symbol of keypad_symbols) {
+            if (!column.includes(keypad_symbol)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static get_ordered_symbols(keypad_symbols, column) {
+        const positions = keypad_symbols.map(keypad_symbol => column.findIndex(keypad_symbol));
+        positions.sort((a, b) => a - b);
+
+        return positions.map(idx => column[idx]);
+    }
+
+    static solve(keypad_symbols) {
+        for (let column of this.#COLUMNS) {
+            if (this.#symbols_in_column(keypad_symbols, column)) {
+                return this.#get_ordered_symbols(keypad_symbols, column);
+            }
+        }
+
+        return undefined;
+    }
+};
---
+++ src/types/keypad_symbol.js
@@ -0,0 +1,37 @@
+import { Enumify } from 'enumify';
+
+export default class KeypadSymbol extends Enumify {
+    static MIRROR                       = new KeypadSymbol();
+    static FUNNY_LETTER_A               = new KeypadSymbol();
+    static LAMBDA                       = new KeypadSymbol();
+    static WEIRD_MIRRORED_N             = new KeypadSymbol();
+    static KITTY                        = new KeypadSymbol();
+    static LETTER_H                     = new KeypadSymbol();
+    static BACKWARD_C_WITH_DOT          = new KeypadSymbol();
+    static BACKWARD_E_WITH_UMLAUT       = new KeypadSymbol();
+    static FANCY_Q                      = new KeypadSymbol();
+    static HOLLOW_STAR                  = new KeypadSymbol();
+    static UPSIDE_DOWN_QUESTION_MARK    = new KeypadSymbol();
+    static COPYRIGHT                    = new KeypadSymbol();
+    static BALLSACK                     = new KeypadSymbol();
+    static MIRRORED_PAIR_OF_KS          = new KeypadSymbol();
+    static WEIRD_SCRIPT_R               = new KeypadSymbol();
+    static WEIRD_6                      = new KeypadSymbol();
+    static BACKWARDS_PARAGRAPH          = new KeypadSymbol();
+    static LETTER_B_WITH_CROSS          = new KeypadSymbol();
+    static SMILEY_FACE                  = new KeypadSymbol();
+    static PSI                          = new KeypadSymbol();
+    static C_WITH_DOT                   = new KeypadSymbol();
+    static THREE_WITH_TAIL              = new KeypadSymbol();
+    static FULL_STAR                    = new KeypadSymbol();
+    static PUZZLE_PIECE                 = new KeypadSymbol();
+    static AE                           = new KeypadSymbol();
+    static FORMAL_MIRRORED_N            = new KeypadSymbol();
+    static OMEGA                        = new KeypadSymbol();
+	
+	static _ = this.closeEnum();
+	
+	static fromString(val) {
+		return KeypadSymbol[val];
+	}
+};
