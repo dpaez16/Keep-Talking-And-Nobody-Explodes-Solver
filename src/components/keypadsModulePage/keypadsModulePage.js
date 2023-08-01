7c085e5e67b7a07853bbf8795ad7adfc57e8271d
Danny Paez <dpaez16@yahoo.com>
2023-08-01 16:02:38 -0700

    keypads module UI improvements

--- src/components/keypadsModulePage/keypadsModulePage.js
+++ src/components/keypadsModulePage/keypadsModulePage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Container, Header, Button, Input, Dropdown, Label, Image} from 'semantic-ui-react';
+import {Container, Header, Button, Image, Divider} from 'semantic-ui-react';
 import KeypadsSolver from '../../module_solvers/keypads';
 import KeypadSymbol from '../../types/keypad_symbol';
 import './keypadsModulePage.css';
@@ -41,7 +41,10 @@ export default class KeypadsModulePage extends Component {
                 {
                     this.state.answer.map((symbol, idx) => {
                         return (
-                            <Image src={require(`./keypad_symbols/${symbol.enumKey.toLowerCase()}.png`)} />
+                            <Image 
+                                src={require(`./keypad_symbols/${symbol.enumKey.toLowerCase()}.png`)} 
+                                bordered
+                            />
                         );
                     })
                 }
@@ -50,34 +53,71 @@ export default class KeypadsModulePage extends Component {
     }
 
     render() {
-        const options = KeypadSymbol.enumValues.map((symbol) => {
-            return {
-                key: symbol.enumKey, 
-                value: symbol, 
-                text: '',
-                image: { avatar: false, src: require(`./keypad_symbols/${symbol.enumKey.toLowerCase()}.png`) }
-            };
-        });
-
         return (
             <Container 
                 className="KeypadsModulePage" 
                 fluid 
             >
                 <Header as='h1'>Keypads</Header>
+                <Container 
+                    className="KeypadsModulePage_symbols"
+                    fluid
+                >
+                    {
+                        KeypadSymbol.enumValues.map((keypad_symbol, idx) => {
+                            return (
+                                <Image  src={require(`./keypad_symbols/${keypad_symbol.enumKey.toLowerCase()}.png`)}
+                                        size="tiny"
+                                        bordered
+                                        key={idx}
+                                        wrapped={true}
+                                        onClick={e => {
+                                            e.preventDefault();
+
+                                            if (this.state.keypadSymbols.length === 4) {
+                                                return;
+                                            }
+
+                                            this.setState({
+                                                keypadSymbols: [...this.state.keypadSymbols, keypad_symbol]
+                                            });
+                                        }}
+                                />
+                            );
+                        })
+                    }
+                </Container>
+                <Divider />
                 <Container fluid>
-                    <Label>Keypad Symbols</Label>
-                    <Dropdown
-                        className="keypadSymbolsDropdown"
-                        placeholder="Keypad Symbol"
-                        name="keypadSymbols"
-                        multiple
-                        selection
-                        options={options}
-                        onChange={this.handleChange.bind(this)}
-                    />
+                    <Header as='h3'>
+                        {this.state.keypadSymbols.length > 0 && "Selected:"}
+                    </Header>
+                    {
+                        this.state.keypadSymbols.map((keypad_symbol, idx) => {
+                            return (
+                                <Image  src={require(`./keypad_symbols/${keypad_symbol.enumKey.toLowerCase()}.png`)}
+                                        size="tiny"
+                                        key={idx}
+                                        wrapped={true}
+                                        bordered
+                                />
+                            );
+                        })
+                    }
                 </Container>
                 <Container fluid>
+                <Button
+                    negative
+                    onClick={e => {
+                        e.preventDefault();
+                        this.setState({
+                            keypadSymbols: [],
+                            answer: undefined
+                        });
+                    }}
+                >
+                    Clear Symbols
+                </Button>
                 <Button
                         positive
                         disabled={!this.inputValid()}
